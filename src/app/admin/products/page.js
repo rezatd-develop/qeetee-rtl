0a2237876693265175e69a925d3c82d65087700c
jack-philips <reza.td.develop@gmail.com>
2024-09-12 14:18:58 -0700

    add basics of grids and tables

--- src/app/admin/products/page.js
+++ src/app/admin/products/page.js
@@ -1,3 +1,36 @@
+'use client'
+
+import AgGridTable from "@/src/components/agGridTable/AgGridTable";
+import { useState } from "react";
+
 export default function AdminProducts() {
-    return <div className="bg-danger h-100 w-100">content</div>
+    const [rowData, setRowData] = useState([
+        { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+        { make: "Ford", model: "F-Series", price: 33850, electric: false },
+        { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+        { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+        { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+        { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+        { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+        { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+        { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+        { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+        { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+        { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+        { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+        { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+        { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+    ]);
+
+    const [colDefs, setColDefs] = useState([
+        { field: "make" },
+        { field: "model" },
+        { field: "price" },
+        { field: "electric" }
+    ]);
+    return <div className="p-3">
+        <div className="fw-bold font-size-1300 bg-dark text-white py-2 px-3 rounded mb-3">Products</div>
+        <AgGridTable rowData={rowData}
+            colDefs={colDefs} />
+    </div>
 }
